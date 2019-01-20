9f364cb9931e9872dc95cbcb7870860f2c52fce7
Maija Haapakangas <mhaapakangas@gmail.com>
2019-01-20 13:42:06 +0200

    Add endpoint to update movies

--- application/movies/views.py
+++ application/movies/views.py
@@ -9,8 +9,13 @@ def movies_index():
 
 
 @app.route("/movies/new/")
-def movies_form():
+def movies_add_form():
     return render_template("movies/new.html")
+
+
+@app.route("/movies/update/<movie_id>/")
+def movies_update_form(movie_id):
+    return render_template("movies/update.html", movie=Movie.query.get(movie_id))
 
 
 @app.route("/movies/", methods=["POST"])
@@ -21,6 +26,19 @@ def movies_create():
                   request.form.get("runtime"))
 
     db.session().add(movie)
+    db.session().commit()
+
+    return redirect(url_for("movies_index"))
+
+
+@app.route("/movies/<movie_id>/", methods=["POST"])
+def movies_update(movie_id):
+    movie = Movie.query.get(movie_id)
+    movie.name = request.form.get("name")
+    movie.year = request.form.get("year")
+    movie.runtime = request.form.get("runtime")
+    movie.genre = request.form.get("genre")
+
     db.session().commit()
 
     return redirect(url_for("movies_index"))
