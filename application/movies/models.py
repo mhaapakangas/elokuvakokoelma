82204804b65a696e47e26887f1a9379e818abfb9
Maija Haapakangas <mhaapakangas@gmail.com>
2019-02-02 16:22:56 +0200

    Add base class for models

--- application/movies/models.py
+++ application/movies/models.py
@@ -1,10 +1,10 @@
 from application import db
+from application.models import Base
 
 
-class Movie(db.Model):
+class Movie(Base):
     __tablename__ = "movie"
 
-    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(144), nullable=False)
     year = db.Column(db.Integer, nullable=False)
     genre = db.Column(db.String(144), nullable=False)
--- application/ratings/models.py
+++ application/ratings/models.py
@@ -1,10 +1,10 @@
 from application import db
+from application.models import Base
 
 
-class Rating(db.Model):
+class Rating(Base):
     __tablename__ = "rating"
 
-    id = db.Column(db.Integer, primary_key=True)
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('account.id'))
     want_to_watch = db.Column(db.Boolean)
