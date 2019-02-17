e82f2c6b84bbd6069ca9e618d527f68eaed855d9
Maija Haapakangas <mhaapakangas@gmail.com>
2019-02-17 18:22:07 +0200

    Add index for rating table

--- application/ratings/models.py
+++ application/ratings/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.ext.declarative.base import declared_attr
+
 from application import db
 from application.models import Base
 
@@ -9,3 +11,7 @@ class Rating(Base):
     user_id = db.Column(db.Integer, db.ForeignKey('account.id'))
     want_to_watch = db.Column(db.Boolean)
     rating = db.Column(db.Integer)
+
+    @declared_attr
+    def __table_args__(cls):
+        return (db.Index("rating_index_%s" % cls.__tablename__, "rating"),)
