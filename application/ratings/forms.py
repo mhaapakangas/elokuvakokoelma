58e1e32d7c13d4c236a2710a00f0b46db1aec102
Maija Haapakangas <mhaapakangas@gmail.com>
2019-02-25 00:01:22 +0200

    Change bootstrap theme

--- application/ratings/forms.py
+++ application/ratings/forms.py
@@ -1,9 +1,10 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, html5
 from wtforms import IntegerField, validators
 
 
 class RatingForm(FlaskForm):
-    rating = IntegerField("My rating:", [validators.Optional(), validators.NumberRange(min=1, max=10)])
+    rating = IntegerField("My rating:", [validators.Optional(), validators.NumberRange(min=1, max=10)],
+                          widget=html5.NumberInput(min=1, max=10))
 
     class Meta:
         csrf = False
